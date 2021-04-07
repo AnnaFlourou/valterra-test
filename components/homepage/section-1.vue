fa6435f6bea1b5d0ea131f04913c0355306af3ab
Robin Palette <anna.flourou@gmail.com>
2021-04-07 14:26:32 +0200

    section 1 layout

--- components/homepage/section-1.vue
+++ components/homepage/section-1.vue
@@ -1,6 +1,20 @@
 <template>
- <div class="section1">
- hello section 1
+ <div class="section1 row">
+    <h2 class="col-12">Du relief <br/> à nos sols</h2>
+    <div class="items ">
+        <div class="item col-4">
+            <h3>01. Notre credo</h3>
+            <p>Acteur de l'économie circulaire</p>
+        </div>
+         <div class="item col-4">
+            <h3>02. Nos métiers groupe</h3>
+            <p>Environnement et traitement des déchets organiques</p>
+        </div>
+         <div class="item col-4">
+            <h3>03. nos implantations</h3>
+            <p>Une relation de proximité</p>
+        </div>
+    </div>
  </div>
 </template>
 
