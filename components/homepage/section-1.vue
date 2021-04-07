e3846ab9cf9c220d248a09b90e7b07ad9f6ad9d6
Robin Palette <anna.flourou@gmail.com>
2021-04-07 14:43:08 +0200

    fix layout issues

--- components/homepage/section-1.vue
+++ components/homepage/section-1.vue
@@ -1,16 +1,16 @@
 <template>
- <div class="section1 row">
-    <h2 class="col-12">Du relief <br/> à nos sols</h2>
-    <div class="items ">
-        <div class="item col-4">
+ <div class="section1">
+    <h2 class="row">Du relief <br/> à nos sols</h2>
+    <div class="items row">
+        <div class="item col-3">
             <h3>01. Notre credo</h3>
             <p>Acteur de l'économie circulaire</p>
         </div>
-         <div class="item col-4">
+         <div class="item col-3">
             <h3>02. Nos métiers groupe</h3>
             <p>Environnement et traitement des déchets organiques</p>
         </div>
-         <div class="item col-4">
+         <div class="item col-3">
             <h3>03. nos implantations</h3>
             <p>Une relation de proximité</p>
         </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,15 @@
 <template>
+<div class="overall">
     <Nuxt />
+</div>
+   
 </template>
 
+<script>
+ import navbar from "@/components/navbar";
+export default {
+   components: {
+          navbar
+        },
+}
+</script>
